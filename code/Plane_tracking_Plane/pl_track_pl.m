5b8cc4b15c85960b8e7ad7c6d398782365b9f699
Mo Chen <mochen72@berkeley.edu>
2016-12-06 15:49:08 -0800

    minor changes

--- code/Plane_tracking_Plane/pl_track_pl.m
+++ code/Plane_tracking_Plane/pl_track_pl.m
@@ -18,15 +18,14 @@ if nargin < 1
 end
 
 %% Virtual Plane
-wMax_virt = 0.6;
-vrange_virt = [2.5 3.5];
-virt_pl = Plane([0;0;0], wMax_virt, vrange_virt);
+wMax_virt = pi/4;
+vrange_virt = [2 3];
+virt_pl = Plane([0;0;0], wMax_virt, vrange_virt, [0 0]);
 
 %% Real Plane
-wMax = pi/2;      % rad/s
-vrange = [0.1 5]; % m/s
+wMax = pi/3;      % rad/s
+vrange = [0.5 5]; % m/s
 dMax = [1 0];     % m/s
-
 real_pl = Plane([0;0;0], wMax, vrange, dMax);
 
 % Pursuit evasion dynamical system
@@ -41,15 +40,15 @@ g = createGrid(grid_min, grid_max, N, 3);
 
 % Track trajectory for up to this time
 tMax = 10; % for SPPwIntruderRTT method 2
-dt = 0.025;
+dt = 0.1;
 tau = 0:dt:tMax;
 
 % Initial conditions
 data0 = -sqrt(g.xs{1}.^2 + g.xs{2}.^2);
 schemeData.dynSys = dynSys;
 schemeData.grid = g;
-schemeData.uMode = 'max';
-schemeData.dMode = 'min';
+schemeData.uMode = 'min';
+schemeData.dMode = 'max';
 extraArgs.visualize = true;
 extraArgs.RS_level = -0.9*L;
 extraArgs.deleteLastPlot = true;
