a14fc494bb65ceb712258ddf7f8a2701af3e0ed3
Mo Chen <mochen72@berkeley.edu>
2017-01-09 20:43:23 -0800

    minor changes to outline of tracking paper

--- code/RRT/run_rrt.m
+++ code/RRT/run_rrt.m
@@ -1,9 +1,4 @@
 function run_rtt()
-treesMax = 28; %How many multiple trees (must be at least 2, 1 for source and 1 for destination
-
-seedsPerAxis = 3; %Number of seeds allowed on each axis (discretely placed seeds which idealy helps the RRT expansion)
-
-wallCount = 1; %Number of mock walls to be placed in the environment
 
 obstacleFilename = 'obstacles.txt';
 seedsPerAxis = 7;
