be49c100b7b476f9c32853b7ac792dff2ddb4e19
Mo Chen <mochen72@gmail.com>
2018-03-25 15:55:44 -0700

    bigger speed cost for Q8D_Q4D

--- code/Precomputation/Q8D_Q4D_RS.m
+++ code/Precomputation/Q8D_Q4D_RS.m
@@ -21,7 +21,7 @@ sD.grid = createGrid(gMin, gMax, gN);
 
 % extraArgs.targets = -sqrt(sD.grid.xs{1}.^2 + sD.grid.xs{2}.^2 / 4);
 
-extraArgs.targets = min(-abs(sD.grid.xs{1}), -abs(sD.grid.xs{2}));
+extraArgs.targets = min(-abs(sD.grid.xs{1}), -abs(sD.grid.xs{2})/2);
 
 
 extraArgs.low_memory = true;
