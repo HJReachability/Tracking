2908ccf630368385508a1378b799a562e47e3274
Mo Chen <mochen72@gmail.com>
2018-05-02 09:35:52 -0700

    revised up to V-B

--- code/Precomputation/Q8D_Q4D_RS_vis.m
+++ code/Precomputation/Q8D_Q4D_RS_vis.m
@@ -2,14 +2,15 @@ function Q8D_Q4D_RS_vis(g, data, tau, level)
 
 f = figure;
 f.Color = 'White';
-f.Position = [100 100 800 600];
+f.Position = [100 0 600 960];
 
 N = 5;
 inds2plot = round(logspace(0, log(length(tau))/log(10), N));
 
 s = cell(N,1);
 c = cell(N,1);
-colors = hot(N+3);
+c2 = cell(N,1);
+colors = winter(N+3);
 legKey = [];
 legVal = cell(N,1);
 
@@ -20,32 +21,51 @@ for ii = 1:N
   [g2D, data2D] = proj(g, data(:,:,:,:,inds2plot(ii)), [0 0 1 1], 'max');
   
   color = colors(ii,:);
+  
+  subplot(2,1,1)
   s{ii} = surf(g2D.vs{1}, g2D.vs{2}, -data2D);
-%   s{ii}.EdgeColor = 'none';
   s{ii}.EdgeAlpha = (0.9*ii/N).^1;
   s{ii}.FaceColor = color;
   s{ii}.FaceAlpha = (0.9*ii/N).^1;
   
-  legKey = [legKey s{ii}];
-  legVal{ii} = sprintf('t = %.2f', max(tau) - tau(inds2plot(ii)));
+  
+  
   hold on
   
   extraArgs.applyLight = false;
   c{ii} = visSetIm(g2D, data2D, color, level, extraArgs);
   c{ii}.LineWidth = 3;
-%   c{ii}.Visible = 'off';
-%   
-%   contourZ = -level*ones(length(c{ii}.ContourMatrix(1,2:end)), 1);
-%   plot3(c{ii}.ContourMatrix(1,2:end), c{ii}.ContourMatrix(2,2:end), ...
-%     contourZ, 'color', colors(ii,:));
-
+  
+  daspect([1 1 0.7])
+  
+  title('$V(r, T-\tau)$', 'Interpreter', 'LaTeX', 'FontSize', 14)
+  xlabel('Velocity', 'FontSize', 14)
+  ylabel('Position', 'FontSize', 14)
+  zlabel('Value', 'FontSize', 14)
+  box on
+  view([-70 13])
+  
+  subplot(2,1,2)
+  c2{ii} = visSetIm(g2D, data2D', color, level, extraArgs);
+  c2{ii}.LineWidth = 3;
+  
+  legKey = [legKey c2{ii}];
+  legVal{ii} = sprintf('\\tau = %.2f', max(tau) - tau(inds2plot(ii)));
+  
+  hold on
+  
+  title('$\mathcal B (r, \tau)$', 'Interpreter', 'LaTeX', 'FontSize', 14)
+  xlabel('Position', 'FontSize', 14)
+  ylabel('Velocity', 'FontSize', 14)
+  grid on
+  box on
+  axis equal
 end
 
+subplot(2,1,1)
+surf(g2D.vs{1}, g2D.vs{2}, -level*ones(size(g2D.xs{1})), 'FaceColor', ...
+  [0.1 0.1 0.1], 'FaceAlpha', 0.1);
 
-daspect([1 1 0.7])
+legend(legKey, legVal, 'FontSize', 14)
 
-legend(legKey, legVal)
-
-xlabel('Velocity', 'FontSize', 14)
-ylabel('Position', 'FontSize', 14)
-zlabel('Value', 'FontSize', 14)
+end
